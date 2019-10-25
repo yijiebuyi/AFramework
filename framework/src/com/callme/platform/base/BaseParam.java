6a1b4ec6ba4096310f7c06bd8f27c0da120caf89
huangyong <huangyong@huwochuxing.com>
2019-10-25 10:36:39 +0800

    [impro]:添加新的参数基类

---
+++ framework/src/com/callme/platform/base/BaseParam.java
@@ -0,0 +1,19 @@
+package com.callme.platform.base;
+
+import java.io.Serializable;
+
+/**
+ * Copyright (C) 2017 重庆呼我出行网络科技有限公司
+ * 版权所有
+ * <p>
+ * 功能描述：
+ * 作者：huangyong
+ * 创建时间：2019/10/25
+ * <p>
+ * 修改人：
+ * 修改描述：
+ * 修改日期
+ */
+public class BaseParam implements Serializable {
+
+}
